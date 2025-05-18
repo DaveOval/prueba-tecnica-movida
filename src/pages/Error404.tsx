619d58558d25141f05e06752738cd013239ed6b1
David Vazquez <dave_u@outlook.com>
2025-05-18 08:34:41 -0600

    feat: update title and enhance Error404 page layout
    - Changed the document title from "VM - Inventario" to "VMS" for better branding.
    - Redesigned the Error404 component to improve user experience with a centered layout and navigation link back to the homepage.

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,3 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export const Error404 = () => {
-  return <div>Error 404: Page Not Found.</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="text-lg text-gray-400">Página no encontrada</p>
+      <Link to="/" className="p-2 rounded-md bg-blue-500 text-white mt-5">
+        Volver a la página de inicio
+      </Link>
+    </div>
+  );
 };
