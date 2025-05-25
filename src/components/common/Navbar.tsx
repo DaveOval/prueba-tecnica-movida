cbee79952397bd5cec34126bae6b572fe3fccbaa
David Vazquez <dave_u@outlook.com>
2025-05-24 21:23:52 -0600

    feat: enhance mobile Navbar with user profile section
    - Added user profile section to the mobile Navbar, including user image and name display.
    - Improved layout for better alignment and user interaction.
    - Removed commented-out notification button for a cleaner codebase.

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -90,8 +90,32 @@ const Navbar = () => {
         </div>
       </header>
       {isOpen && isMobile && (
-        <div className="h-16 bg-white border-b border-gray-200 px-6 shadow-md">
-          <h1>Hola</h1>
+        <div className="h-16 bg-white border-b border-gray-200 px-6 shadow-md flex flex-row items-center justify-end">
+          {/* Notification and user info */}
+          <section className="flex flex-row items-center gap-3">
+            {/* <div>
+                <button className="cursor-pointer border rounded-4xl p-3 border-gray-300 text-gray-500">
+                  <IconNotification size={20} />
+                </button>
+              </div> */}
+            <div className="flex flex-row items-center gap-2">
+              <button className="cursor-pointer border rounded-4xl p-3 border-gray-300 text-gray-500">
+                {user?.image ? (
+                  <img
+                    src={user.image}
+                    alt="User profile"
+                    className="w-8 h-8 rounded-full object-cover"
+                  />
+                ) : (
+                  <IconUser size={20} />
+                )}
+              </button>
+              <button className="text-gray-500 text-md cursor-pointer flex flex-row items-center justify-center gap-1">
+                {user?.name ? user.name : 'Nombre'}
+                <IconArrowDown size={22} />
+              </button>
+            </div>
+          </section>
         </div>
       )}
     </>
