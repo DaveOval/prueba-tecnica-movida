65c66e79bb98ccb73f7e69ad9b3606079585184a
David Vazquez <dave_u@outlook.com>
2025-05-31 17:49:28 -0600

    refactor: enhance EditProduct form layout for improved responsiveness and organization
    - Reorganized input fields into grid layouts for better responsiveness.
    - Added additional structure to the form, grouping related fields together.
    - Included a console log for debugging purposes when fetching product data.

--- src/pages/products/EditProduct.tsx
+++ src/pages/products/EditProduct.tsx
@@ -25,7 +25,7 @@ interface EditProductFormData {
 
 export const EditProduct = () => {
   const { id } = useParams<{ id: string }>();
-  
+
   const {
     register,
     handleSubmit,
@@ -43,9 +43,11 @@ export const EditProduct = () => {
   const isBatchTracked = watch('is_batch_tracked');
   const isExpiryTracked = watch('is_expiry_tracked');
 
-  const { isLoading, error, getProductAction, updateProductAction } = useEditProduct(id || '');
+  const { isLoading, error, getProductAction, updateProductAction } =
+    useEditProduct(id || '');
 
   useEffect(() => {
+    console.log('id', id);
     const fetchProduct = async () => {
       try {
         const data = await getProductAction();
@@ -72,117 +74,131 @@ export const EditProduct = () => {
     <FormLayout title="Editar producto">
       <FormContainer title="Edita la información del producto">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            label="Nombre"
-            id="name"
-            placeholder="Nombre del producto"
-            required
-            type="text"
-            error={errors.name?.message}
-            {...register('name')}
-          />
-          <Input
-            label="Descripción"
-            id="description"
-            placeholder="Descripción del producto"
-            required
-            type="text"
-            error={errors.description?.message}
-            {...register('description')}
-          />
-          <Input
-            label="Categoría"
-            id="category"
-            placeholder="Categoría del producto"
-            required
-            type="text"
-            error={errors.category?.message}
-            {...register('category')}
-          />
-          <Input
-            label="Unidad de medida"
-            id="unit_of_measure"
-            placeholder="Unidad de medida del producto"
-            required
-            type="text"
-            error={errors.unit_of_measure?.message}
-            {...register('unit_of_measure')}
-          />
-          <Input
-            label="Código de barras"
-            id="barcode"
-            placeholder="Código de barras del producto"
-            required
-            type="text"
-            error={errors.barcode?.message}
-            {...register('barcode')}
-          />
-          <ToggleSwitch
-            checked={isBatchTracked}
-            onChange={(value) => setValue('is_batch_tracked', value)}
-            title="Es un lote"
-            required
-            error={errors.is_batch_tracked?.message}
-          />
-          <ToggleSwitch
-            checked={isExpiryTracked}
-            onChange={(value) => setValue('is_expiry_tracked', value)}
-            title="Es vencimiento"
-            required
-            error={errors.is_expiry_tracked?.message}
-          />
-          <Input
-            label="Nivel mínimo de stock"
-            id="min_stock_level"
-            placeholder="Nivel mínimo de stock"
-            required
-            type="number"
-            error={errors.min_stock_level?.message}
-            {...register('min_stock_level')}
-          />
-          <Input
-            label="Nivel máximo de stock"
-            id="max_stock_level"
-            placeholder="Nivel máximo de stock"
-            required
-            type="number"
-            error={errors.max_stock_level?.message}
-            {...register('max_stock_level')}
-          />
-          <Input
-            label="Ubicación por defecto"
-            id="default_location"
-            placeholder="Ubicación por defecto"
-            required
-            type="text"
-            error={errors.default_location?.message}
-            {...register('default_location')}
-          />
-          <Input
-            label="Proveedor"
-            id="supplier_id"
-            placeholder="Proveedor"
-            required
-            type="text"
-            error={errors.supplier_id?.message}
-            {...register('supplier_id')}
-          />
-          <Input
-            label="Precio"
-            id="price"
-            placeholder="Precio"
-            required
-            type="number"
-            error={errors.price?.message}
-            {...register('price')}
-          />
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <Input
+              label="Nombre"
+              id="name"
+              placeholder="Nombre del producto"
+              required
+              type="text"
+              error={errors.name?.message}
+              {...register('name')}
+            />
+            <Input
+              label="Descripción"
+              id="description"
+              placeholder="Descripción del producto"
+              required
+              type="text"
+              error={errors.description?.message}
+              {...register('description')}
+            />
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <Input
+              label="Categoría"
+              id="category"
+              placeholder="Categoría del producto"
+              required
+              type="text"
+              error={errors.category?.message}
+              {...register('category')}
+            />
+            <Input
+              label="Unidad de medida"
+              id="unit_of_measure"
+              placeholder="Unidad de medida del producto"
+              required
+              type="text"
+              error={errors.unit_of_measure?.message}
+              {...register('unit_of_measure')}
+            />
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <Input
+              label="Código de barras"
+              id="barcode"
+              placeholder="Código de barras del producto"
+              required
+              type="text"
+              error={errors.barcode?.message}
+              {...register('barcode')}
+            />
+            <Input
+              label="Nivel mínimo de stock"
+              id="min_stock_level"
+              placeholder="Nivel mínimo de stock"
+              required
+              type="number"
+              error={errors.min_stock_level?.message}
+              {...register('min_stock_level')}
+            />
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <Input
+              label="Nivel máximo de stock"
+              id="max_stock_level"
+              placeholder="Nivel máximo de stock"
+              required
+              type="number"
+              error={errors.max_stock_level?.message}
+              {...register('max_stock_level')}
+            />
+            <Input
+              label="Ubicación por defecto"
+              id="default_location"
+              placeholder="Ubicación por defecto"
+              required
+              type="text"
+              error={errors.default_location?.message}
+              {...register('default_location')}
+            />
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <Input
+              label="Proveedor"
+              id="supplier_id"
+              placeholder="Proveedor"
+              required
+              type="text"
+              error={errors.supplier_id?.message}
+              {...register('supplier_id')}
+            />
+            <Input
+              label="Precio"
+              id="price"
+              placeholder="Precio"
+              required
+              type="number"
+              error={errors.price?.message}
+              {...register('price')}
+            />
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <ToggleSwitch
+              checked={isBatchTracked}
+              onChange={(value) => setValue('is_batch_tracked', value)}
+              title="Es un lote"
+              required
+              error={errors.is_batch_tracked?.message}
+            />
+            <ToggleSwitch
+              checked={isExpiryTracked}
+              onChange={(value) => setValue('is_expiry_tracked', value)}
+              title="Es vencimiento"
+              required
+              error={errors.is_expiry_tracked?.message}
+            />
+          </div>
           {error && <p className="text-red-500">{error}</p>}
-          <SubmitButton
-            type="submit"
-            label="Guardar cambios"
-            loading={isLoading}
-            disabled={isLoading}
-          />
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 mt-4">
+            <SubmitButton
+              type="submit"
+              label="Guardar cambios"
+              loading={isLoading}
+              disabled={isLoading}
+            />
+          </div>
         </form>
       </FormContainer>
     </FormLayout>
