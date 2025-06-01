a161ef49c7d7c30391e4d35b2fa19412a6a9c980
David Vazquez <dave_u@outlook.com>
2025-06-01 17:09:13 -0600

    refactor: update Icon components for consistency and improved styling
    - Changed stroke attributes in IconAdd component to camelCase for consistency with JSX standards.
    - Reformatted IconStock component for better readability and updated stroke attributes to camelCase.
    - Ensured consistent use of JSX syntax across both icon components.

--- src/assets/icons/IconAdd.tsx
+++ src/assets/icons/IconAdd.tsx
@@ -13,9 +13,9 @@ export const IconAdd = ({ size, style }: IconProps) => {
         <path
           d="M12 5V19M5 12H19"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     </div>
