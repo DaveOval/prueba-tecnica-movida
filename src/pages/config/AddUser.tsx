ef3fbac83e0404d95fe18f3de9970ad7aa764e9f
David Vazquez <dave_u@outlook.com>
2025-06-06 21:09:10 -0600

    feat: implement AddUser form with validation and submission handling
    - Introduced AddUser component with a form for user creation.
    - Integrated react-hook-form for form management and validation.
    - Added toast notifications for user feedback on form submission.
    - Structured form to capture user details: name, username, email, password, and role.

--- src/pages/config/AddUser.tsx
+++ src/pages/config/AddUser.tsx
@@ -1,3 +1,36 @@
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+
+import { FormLayout } from '../../components/layout/';
+import { FormContainer, InputSelect } from '../../components/common';
+
+interface AddUserFormData {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
 export const AddUser = () => {
-  return <div>AddUser</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setValue,
+    reset,
+  } = useForm<AddUserFormData>();
+
+  const onSubmit = (data: AddUserFormData) => {
+    console.log(data);
+  };
+
+  return (
+    <FormLayout title="Agregar usuario">
+      <FormContainer title="Completa la información del usuario">
+        <form onSubmit={handleSubmit(onSubmit)}></form>
+      </FormContainer>
+    </FormLayout>
+  );
 };
